7e38fa14b7508cf8e4fc3a8946c53f566ccf2de4
JOAO VICTOR ALVES DOS SANTOS <hyperlobe2@gmail.com>
2023-09-20 17:23:21 -0300

    tutorial 2

--- src/redux/user/reducer.js
+++ src/redux/user/reducer.js
@@ -1,5 +1,16 @@
-const userReducer = ( ) => {
+const initialState =
+{
+    currentUser : null
+}
 
+
+
+const userReducer = ( state = initialState, action) => {
+    if (action.type === 'user/login') {
+        return {...state, currentUser : 10}
+
+    }
+return state
 }
 
 export default userReducer
